3a05de3b24317f30664c2e1b3397bb5ced2e3897
SRI GOKULA VISHNU B <161995942+srigokulavishnu@users.noreply.github.com>
2025-05-30 13:23:55 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  age: { type: Number },
-  phone: { type: String },
-  gender: { type: String },
-  photo: { type: String }, // Stores the filename of the uploaded photo
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  age: Number,
+  phone: String,
+  gender: String,
+  photo: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
